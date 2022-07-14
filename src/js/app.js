6fafb13217afcf32e5a8d25cb9081770b86d2e6c
Krzysztof Furtak <kfurtak1024@gmail.com>
2022-07-14 12:30:17 +0200

    Add responsive menu for mobiles

--- src/js/app.js
+++ src/js/app.js
@@ -1,62 +1,96 @@
-function updateNavItem(index, active) {
-  function getNavItem(index) {
-    if (index === undefined) {
-      return undefined;
-    }
-    var a = document.querySelectorAll('#nav-menu a.nav-menu-item')[index - 1];
-    return a ? a.parentNode : undefined;
-  }
-  var navItem = getNavItem(index);
-  if (navItem) {
-    if (active) {
-      navItem.classList.add('active');
+(function() {
+  "use strict";
+
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
     } else {
-      navItem.classList.remove('active');
+      return document.querySelector(el)
     }
   }
-}
 
-var myFullpage = new fullpage('#fullpage', {
-  licenseKey:'YOUR_KEY_HERE',
-  slidesNavigation: true,
-  navigation: true,
-  navigationPosition: 'right',
-  navigationTooltips: ['HOME', 'CONTACT'],
-  onLeave: function(origin, destination, direction) {
-    updateNavItem(origin.index + 1, false);
-    updateNavItem(destination.index + 1, true);
-  },
-  afterRender: function() {
-    document.querySelectorAll('#nav-menu a.nav-menu-item')
-      .forEach(function(menuItem, index) {
-        menuItem.addEventListener(
-          'click',
-          function(e) {
-            e.preventDefault();
-            fullpage_api.moveTo(index + 1);
-          });
-      });
-    updateNavItem(1, true);
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
   }
-});
 
-function changeEmailVisibility() {
-  var emailDisplayed = false;
-  return function(e) {
-    if (emailDisplayed === true) {
-      return;
+  const toggleNavItem = (index) => {
+    const navItemAt = (index) => {
+      if (index === undefined) {
+        return undefined
+      }
+
+      let a = select('#nav-menu a.nav-menu-item', true)[index]
+      return a ? a.parentNode : undefined
     }
 
-    e.preventDefault();
-    var email = atob('Y29udGFjdEBrcnp5c3p0b2ZmdXJ0YWsuZGV2Cg==');
-    document.querySelector('#email').textContent = email;
-    var link = document.querySelector('#email-button');
-    link.href='mailto:' + email;
-    link.classList.add('email-visible');
-    emailDisplayed = true;
+    let navItem = navItemAt(index)
+    if (navItem) {
+      navItem.classList.toggle('active')
+    }
   }
-}
 
-document.querySelector('#email-button').addEventListener(
-  'click', changeEmailVisibility()
-);
+  function changeEmailVisibility() {
+    var emailDisplayed = false
+    return function(e) {
+      if (emailDisplayed === true) {
+        return
+      }
+
+      e.preventDefault();
+      let email = atob('Y29udGFjdEBrcnp5c3p0b2ZmdXJ0YWsuZGV2Cg==')
+      let link = select('#email-button')
+      select('#email').textContent = email
+      link.href = 'mailto:' + email
+      link.classList.add('email-visible')
+      emailDisplayed = true
+    }
+  }
+
+  on('click', '#email-button', changeEmailVisibility())
+
+  const toggleMobileMenu = () => {
+    select('#navbar .mobile-nav-toggle .icon', true).forEach(el => {
+      el.classList.toggle('hidden')
+    })
+    select('#navbar').classList.toggle('navbar-mobile')
+  }
+
+  on('click', '.mobile-nav-toggle', () => toggleMobileMenu())
+
+  let myFullpage = new fullpage('#fullpage', {
+    licenseKey:'YOUR_KEY_HERE',
+    slidesNavigation: true,
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['HOME', 'CONTACT'],
+    onLeave: function(origin, destination, direction) {
+      toggleNavItem(origin.index)
+      toggleNavItem(destination.index)
+
+      if (select('#navbar').classList.contains('navbar-mobile')) {
+        toggleMobileMenu()
+      }
+    },
+    afterRender: function() {
+      select('#nav-menu a.nav-menu-item', true)
+        .forEach(function(menuItem, index) {
+          menuItem.addEventListener(
+            'click',
+            function(e) {
+              e.preventDefault()
+              fullpage_api.moveTo(index + 1)
+            })
+        })
+      toggleNavItem(0)
+    }
+  })
+
+})()
